1a27ec1b96d634006db384a5d906b5ab9a444a30
laposhko <laposhko0@gmail.com>
2024-01-30 13:54:02 +0200

    task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,18 @@
-
+const form = document.querySelector(".login-form");
+const formElements = form.elements;
+const loginDetails = {};
+function formSubmit(event) {
+  event.preventDefault();
+  if (
+    formElements[0].value.trim() === "" ||
+    formElements[1].value.trim() === ""
+  ) {
+    alert("All form fields must be filled in");
+  } else {
+    loginDetails[formElements[0].name] = formElements[0].value.trim();
+    loginDetails[formElements[1].name] = formElements[1].value.trim();
+    console.log(loginDetails);
+    form.reset();
+  }
+}
+form.addEventListener("submit", formSubmit);
