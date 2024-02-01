dd3b82193f961dd35e9f4540819be9710eef6ccd
laposhko <laposhko0@gmail.com>
2024-02-01 13:54:47 +0200

    task5

--- js/task-4.js
+++ js/task-4.js
@@ -1,18 +1,29 @@
 const form = document.querySelector(".login-form");
-const formElements = form.elements;
-const loginDetails = {};
 function formSubmit(event) {
   event.preventDefault();
   if (
-    formElements[0].value.trim() === "" ||
-    formElements[1].value.trim() === ""
+    event.target.email.value.trim() === "" ||
+    event.target.password.value.trim() === ""
   ) {
     alert("All form fields must be filled in");
   } else {
-    loginDetails[formElements[0].name] = formElements[0].value.trim();
-    loginDetails[formElements[1].name] = formElements[1].value.trim();
+    const loginDetails = {
+      [event.target.email.name]: event.target.email.value.trim(),
+      [event.target.password.name]: event.target.password.value.trim(),
+    };
     console.log(loginDetails);
-    form.reset();
   }
+
+  // if (
+  //   formElements[0].value.trim() === "" ||
+  //   formElements[1].value.trim() === ""
+  // ) {
+  //   alert("All form fields must be filled in");
+  // } else {
+  //   loginDetails[formElements[0].name] = formElements[0].value.trim();
+  //   loginDetails[formElements[1].name] = formElements[1].value.trim();
+  //   console.log(loginDetails);
+  //   form.reset();
+  // }
 }
 form.addEventListener("submit", formSubmit);
--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+function changeColor() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+}
+button.addEventListener("click", changeColor);
